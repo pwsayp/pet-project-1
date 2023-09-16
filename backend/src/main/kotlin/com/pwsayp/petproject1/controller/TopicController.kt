6526ebd819906c8248e45422c848ba866d4d8d30
a.varganov <avarganov@digitalpanorama.ru>
2023-09-16 22:25:59 +0300

    add routing, header & footer

--- backend/src/main/kotlin/com/pwsayp/petproject1/controller/TopicController.kt
+++ backend/src/main/kotlin/com/pwsayp/petproject1/controller/TopicController.kt
@@ -6,6 +6,7 @@ import com.pwsayp.petproject1.model.dto.TopicPutDto
 import com.pwsayp.petproject1.service.TopicService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/topics")
 class TopicController(
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/main/navbar/navbar.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './components/content/home/home.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
